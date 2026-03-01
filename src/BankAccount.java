b63d6b5cd8315d107eca9b852b4e2117176f37f3
BuildTools <unconfigured@null.spigotmc.org>
2026-03-01 13:02:56 -0700

    updated bank account

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,13 +1,27 @@
 import java.io.FileNotFoundException;
 
 public class BankAccount {
-	private int _balance;
+	private double _balance;
 	
-	public BankAccount(int balance) {
+	public BankAccount(double balance) {
 		this._balance = balance;
 	}
 	
+	/**
+	 * Withdraw a specified amount, or throw an exception if there is not enough to withdraw
+	 * 
+	 * @param amount - the amount to attempt to withdraw
+	 */
 	public void withdraw(double amount) throws NegativeBalanceException, FileNotFoundException {
 		if (amount > _balance) throw new NegativeBalanceException(_balance - amount);
+		
+		amount = _balance;
 	}
+	
+	public void quickWithdraw(double amount) throws NegativeBalanceException, FileNotFoundException {
+		if (amount > _balance) throw new NegativeBalanceException();
+		
+		amount = _balance;
+	}
+	
 }
